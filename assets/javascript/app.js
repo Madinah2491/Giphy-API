eb8b75c02747472411646e91266bcfd3efe4e4d0
Madinah2491 <madinah2491@gmail.com>
2018-03-25 16:05:31 -0400

    changed the api/.get

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,49 +4,59 @@
 //4. when button is clicked 10 static gifs appear
 //5. when gif is clicked it should animate
 //6. under each gif a rating should show provided by giphy api
-
+$(document).ready(function(){
 //1. an array of strings each index is a button on html
-var topics= ["happiness", "meditation", "travel", "laughing"];
+    var topics= ["happiness", "meditation", "travel", "laughing"];
+    function reDraw(){
 
-//makes buttons clickable
-$("#button").on("click", function() {
+    //makes buttons clickable
+    $("#button").on("click", function() {
 
-    //this signifies the button being clicked
-    var topic = $(this).attr("data-topic");
+    })    //this signifies the button being clicked
+        var action = $(this).attr("data-action");
 
-    //var queryURL = "http://api.giphy.com/v1/gifs/search?q=happiness&yapi_key=CqFL5BMF7KOPEkwnQGLYb4UUAz0rnbKj";
-    //    queryURL.done(function(data) { console.log("success got data", data); });
+    }
 
     //})
     // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=CqFL5BMF7KOPEkwnQGLYb4UUAz0rnbKj";
 
-     $.ajax({
-         url: queryURL,
-         method: "GET"
-     })
-         .then(function(response){
-             console.log(queryURL)
-     }
-         )
-             var results = response.data;
+    
+    var xhr = $.get("https://api.giphy.com/v1/gifs/search?=" + topics + "&api_key=CqFL5BMF7KOPEkwnQGLYb4UUAz0rnbKj&q=&limit=10&rating=G");
+        xhr.done(function(data) { console.log("success got data", data); });
 
-     //loop that appends a button for each string
-     for (var i=0; i < topics.length; i++);
 
-     $("#button").append(topics.length)
-             for (var i = 0; i < results.length; i++) {
-             var gifDiv = $("<div class='item'>");
-            
-             var rating = results[i].rating;
-                 ///rating
-             var p = $("<p>").text("Rating: " + rating);
+    newFunction(xhr)
+    .then(function(response){
+        console.log(xhr)
+        
+}
+)
+    var results = response.data;
 
-             var personImage = $("<img>");
-             Image.attr("src", results[i].images.fixed_height.url);
+//loop that appends a button for each string
+for (var i=0; i < topics.length; i++);
 
-             gifDiv.append(p);
-             gifDiv.append(topicsImage);
+$("#button").append(topics.length)
+    for (var i = 0; i < results.length; i++) {
+    var gifDiv = $("<div class='item'>");
+   
+    var rating = results[i].rating;
+        ///rating
+    var p = $("<p>").text("Rating: " + rating);
 
-             $("#gifs-appear-here").append(gifDiv);
-             }
-         });
+    var personImage = $("<img>");
+    Image.attr("src", results[i].images.fixed_height.url);
+
+    gifDiv.append(p);
+    gifDiv.append(topicsImage);
+
+    $("#gifs-appear-here").append(gifDiv);
+    }
+}) 
+
+function newFunction(xhr) {
+    return $.ajax({
+        url: xhr,
+        method: "GET"
+    });
+}
